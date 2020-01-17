5b196e3aca4945eee905eba632755bf7bc0b4e1d
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-01-17 09:34:12 +0200

    Add Header to dist

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,5 @@ import RadioButton from "./components/00-atoms/RadioButton/RadioButton";
 import ConfirmDialog from "./components/02-organisms/ConfirmDialog";
 import RadioButtonWithLabel from "./components/01-molecules/RadioButtonWithLabel";
 import DialogTitle from "./components/02-organisms/DialogTitle";
-export { Checkbox, RadioButton, ConfirmDialog, RadioButtonWithLabel, DialogTitle };
+import Header from "./components/02-organisms/Header";
+export { Checkbox, RadioButton, ConfirmDialog, RadioButtonWithLabel, DialogTitle, Header };
