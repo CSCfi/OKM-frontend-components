8cd167a1637091139ae72e45ea204f6abd1b10ab
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-01-28 07:30:40 +0200

    Update TextBox with changes from oiva-frontend

--- src/lib/components/00-atoms/TextBox/index.js
+++ src/lib/components/00-atoms/TextBox/index.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import PropTypes from "prop-types";
+import Tooltip from "../../02-organisms/Tooltip";
+import { isEmpty } from "ramda";
+import HelpIcon from "@material-ui/icons/Help";
+
+import styles from "./textbox.module.css";
 
 const TextBox = props => {
   const [value, setValue] = useState(null);
@@ -24,7 +29,7 @@ const TextBox = props => {
     if (props.value !== value || !value) {
       setValue(props.value);
     }
-  }, [props.value, value]);
+  }, [props.value]);
 
   return (
     <React.Fragment>
@@ -32,30 +37,52 @@ const TextBox = props => {
         <React.Fragment>
           {props.title && (
             <label className="text-bold text-base block my-2">
+              {props.isRequired && (
+                <span
+                  className={`text-${
+                    props.isValid ? "green" : "red"
+                  }-500 text-2xl pr-4`}>
+                  *
+                </span>
+              )}
               {props.title}
             </label>
           )}
-          <TextareaAutosize
-            aria-label={props.ariaLabel}
-            disabled={props.isDisabled || props.isReadOnly}
-            id={props.id}
-            placeholder={
-              props.isDisabled || props.isReadOnly ? "" : props.placeholder
-            }
-            rows={props.isReadOnly ? 1 : props.rows}
-            rowsMax={props.isReadOnly ? Infinity : props.rowsMax}
-            className={`${props.isHidden ? "hidden" : ""}
-             ${
-               props.isReadOnly
-                 ? "color: text-black"
-                 : "border border-solid border-gray-500 rounded"
-             } w-full p-2 resize-none`}
-            onChange={updateValue}
-            value={value}
-            inputprops={{
-              readOnly: props.isReadOnly
-            }}
-          />
+          <div className="flex">
+            <TextareaAutosize
+              aria-label={props.ariaLabel}
+              disabled={props.isDisabled || props.isReadOnly}
+              id={props.id}
+              placeholder={
+                props.isDisabled || props.isReadOnly ? "" : props.placeholder
+              }
+              rows={props.isReadOnly ? 1 : props.rows}
+              rowsMax={props.isReadOnly ? Infinity : props.rowsMax}
+              className={`${props.isHidden ? "hidden" : ""} ${
+                props.isReadOnly
+                  ? "text-black"
+                  : "border border-solid border-gray-500 rounded"
+              }
+            w-full p-2 resize-none`}
+              onChange={updateValue}
+              value={value}
+              inputprops={{
+                readOnly: props.isReadOnly
+              }}
+            />
+            {!isEmpty(props.tooltip) && (
+              <div className="ml-8">
+                <Tooltip tooltip={props.tooltip.text} trigger="click">
+                  <HelpIcon
+                    classes={{
+                      colorPrimary: styles.tooltipBg
+                    }}
+                    color="primary"
+                  />
+                </Tooltip>
+              </div>
+            )}
+          </div>
         </React.Fragment>
       ) : null}
     </React.Fragment>
@@ -70,9 +97,12 @@ TextBox.defaultProps = {
   payload: {},
   placeholder: "",
   isReadOnly: false,
+  isRequired: false,
+  isValid: true,
   rows: 2,
   rowsMax: 100,
-  title: ""
+  title: "",
+  tooltip: {}
 };
 
 TextBox.propTypes = {
@@ -86,10 +116,14 @@ TextBox.propTypes = {
   /** Custom object defined by user. */
   payload: PropTypes.object,
   placeholder: PropTypes.string,
+  isErroneous: PropTypes.bool,
   isReadOnly: PropTypes.bool,
+  isRequired: PropTypes.bool,
+  isValid: PropTypes.bool,
   rows: PropTypes.number,
   rowsMax: PropTypes.number,
   title: PropTypes.string,
+  tooltip: PropTypes.object,
   value: PropTypes.string
 };
 
