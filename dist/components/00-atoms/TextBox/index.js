e8e0ccdd7ba61c1af853c64998e8043bf3d9bcda
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-01-28 07:31:42 +0200

    Build and update version

--- dist/components/00-atoms/TextBox/index.js
+++ dist/components/00-atoms/TextBox/index.js
@@ -1,6 +1,10 @@
 import _slicedToArray from "@babel/runtime/helpers/esm/slicedToArray";
 import React, { useState, useEffect } from "react";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import Tooltip from "../../02-organisms/Tooltip";
+import { isEmpty } from "ramda";
+import HelpIcon from "@material-ui/icons/Help";
+import styles from "./textbox.module.css";
 
 var TextBox = function TextBox(props) {
   var _useState = useState(null),
@@ -33,23 +37,37 @@ var TextBox = function TextBox(props) {
     if (props.value !== value || !value) {
       setValue(props.value);
     }
-  }, [props.value, value]);
+  }, [props.value]);
   return React.createElement(React.Fragment, null, value !== null ? React.createElement(React.Fragment, null, props.title && React.createElement("label", {
     className: "text-bold text-base block my-2"
-  }, props.title), React.createElement(TextareaAutosize, {
+  }, props.isRequired && React.createElement("span", {
+    className: "text-".concat(props.isValid ? "green" : "red", "-500 text-2xl pr-4")
+  }, "*"), props.title), React.createElement("div", {
+    className: "flex"
+  }, React.createElement(TextareaAutosize, {
     "aria-label": props.ariaLabel,
     disabled: props.isDisabled || props.isReadOnly,
     id: props.id,
     placeholder: props.isDisabled || props.isReadOnly ? "" : props.placeholder,
     rows: props.isReadOnly ? 1 : props.rows,
     rowsMax: props.isReadOnly ? Infinity : props.rowsMax,
-    className: "".concat(props.isHidden ? "hidden" : "", "\n             ").concat(props.isReadOnly ? "color: text-black" : "border border-solid border-gray-500 rounded", " w-full p-2 resize-none"),
+    className: "".concat(props.isHidden ? "hidden" : "", " ").concat(props.isReadOnly ? "text-black" : "border border-solid border-gray-500 rounded", "\n            w-full p-2 resize-none"),
     onChange: updateValue,
     value: value,
     inputprops: {
       readOnly: props.isReadOnly
     }
-  })) : null);
+  }), !isEmpty(props.tooltip) && React.createElement("div", {
+    className: "ml-8"
+  }, React.createElement(Tooltip, {
+    tooltip: props.tooltip.text,
+    trigger: "click"
+  }, React.createElement(HelpIcon, {
+    classes: {
+      colorPrimary: styles.tooltipBg
+    },
+    color: "primary"
+  }))))) : null);
 };
 
 TextBox.defaultProps = {
@@ -60,8 +78,11 @@ TextBox.defaultProps = {
   payload: {},
   placeholder: "",
   isReadOnly: false,
+  isRequired: false,
+  isValid: true,
   rows: 2,
   rowsMax: 100,
-  title: ""
+  title: "",
+  tooltip: {}
 };
 export default TextBox;
