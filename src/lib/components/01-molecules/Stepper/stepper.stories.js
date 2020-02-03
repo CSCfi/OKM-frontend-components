71e2d1ff39228053623746e39bed0c5dd8fc7406
Sami Heikkinen <sami.heikkinen@valamis.com>
2020-02-03 13:30:10 +0200

    Added storybook-state addon to able to use states in stories. stepper.story to change steps by buttons.

--- src/lib/components/01-molecules/Stepper/stepper.stories.js
+++ src/lib/components/01-molecules/Stepper/stepper.stories.js
@@ -2,52 +2,46 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import StepperNavigation from "./index";
 import { withInfo } from "@storybook/addon-info";
+import { withState } from "@dump247/storybook-state";
 
 const props = [
   {
     title: "Step 1",
-    isFailed: true,
-    onChange: () => console.log("Clicked 1")
+    isFailed: true
   },
   {
     title: "Step 2",
-    isCompleted: true,
-    onChange: () => console.log("Clicked 2")
+    isCompleted: true
   },
   { title: "Step 3", onChange: () => console.log("Clicked 3") }
 ];
 
-let currentStep = 0;
-
-let onHandleStepChange = () => {
-  console.log("Clicked:" + currentStep);
-};
-
 storiesOf("StepperNavigation", module)
   .addDecorator(withInfo)
-  .add("Stepper example", () => (
-    <div>
-      <br />
-      <StepperNavigation
-        name="example"
-        stepProps={props}
-        activeStep={currentStep}
-        handleStepChange={onHandleStepChange}
-      />
-      {/* <button
-        onClick={() => {
-          currentStep--;
-          console.log(currentStep);
-        }}>
-        Prev
-      </button>
-      |
-      <button
-        onClick={() => {
-          currentStep++;
-          console.log(currentStep);
-        }}>
-        Next
-      </button> */}
-    </div>
-  ));
+  .add(
+    "Stepper example",
+    withState({ currentStep: 0 })(({ store }) => (
+      <div>
+        <br />
+        <StepperNavigation
+          name="example"
+          stepProps={props}
+          activeStep={store.state.currentStep}
+          handleStepChange={step => store.set({ currentStep: step - 1 })}
+        />
+        <button
+          onClick={() => {
+            store.set({ currentStep: store.state.currentStep - 1 });
+          }}>
+          - Prev
+        </button>
+        |
+        <button
+          onClick={() => {
+            store.set({ currentStep: store.state.currentStep + 1 });
+          }}>
+          Next +
+        </button>
+      </div>
+    ))
+  );
