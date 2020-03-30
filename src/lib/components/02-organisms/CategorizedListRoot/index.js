37cd4df0c8895d6c87647d78a7bcecd70cdb9a00
Raimo Pitkänen <rapitkan@gmail.com>
2020-03-30 13:26:21 +0300

    More code comments added.

--- src/lib/components/02-organisms/CategorizedListRoot/index.js
+++ src/lib/components/02-organisms/CategorizedListRoot/index.js
@@ -4,21 +4,22 @@ import CategorizedList from "./CategorizedList";
 import { handleNodeMain, getReducedStructure, getTargetNode } from "./utils";
 import * as R from "ramda";
 
+// Default values for some properties of the component.
 const defaultProps = {
   anchor: "anchornamemissing",
   categories: [],
   changes: [null],
-  debug: false,
-  sectionId: "sectionidmissing",
   showCategoryTitles: false
 };
 
+/**
+ * CategorizedListRoot is the entry point of form handling.
+ */
 const CategorizedListRoot = React.memo(
   ({
     anchor = defaultProps.anchor,
     categories = defaultProps.categories,
     changes = defaultProps.changes,
-    debug = defaultProps.debug,
     onUpdate,
     showCategoryTitles = defaultProps.showCategoryTitles,
     isReadOnly,
@@ -30,19 +31,36 @@ const CategorizedListRoot = React.memo(
       return changes;
     }, [changes]);
 
+    /**
+     * Categories (lomake) can be a multidimensional array. It's practical to
+     * reduce the structure into a one dimensional array for use. The reduced
+     * structure is used on defining the updated array of change objects.
+     */
     const reducedStructure = useMemo(() => {
       return getReducedStructure(categories);
     }, [categories]);
 
+    /**
+     * Function will be called when something changes on the form. The only
+     * parameter is changeObj that contains the changed properties and maybe
+     * some metadata too.
+     */
     const onChangesUpdate = useCallback(
       changeObj => {
+        // Target node is the component affected by the change.
         const targetNode = getTargetNode(changeObj, reducedStructure);
+        // The array of change objects will be updated.
         const nextChanges = handleNodeMain(
           targetNode,
           anchor,
           reducedStructure,
           changesRef.current
         );
+        /**
+         * The updated array will be sent using the onUpdate callback function.
+         * The anchor parameter is the root anchor of the current form. It can
+         * be used to bind and store the array of changes correctly.
+         **/
         onUpdate({
           anchor,
           changes: nextChanges
@@ -70,8 +88,16 @@ const CategorizedListRoot = React.memo(
 
     return (
       <React.Fragment>
-        {!R.equals(R.head(changes), null)
+        {/**
+         * If the first change object is not null (default) the CategorizedList
+         * will be created.
+         **/
+        !R.equals(R.head(changes), null)
           ? (() => {
+              /**
+               * This is the first instance of CategorizedList. The component
+               * will create more instances on it's own.
+               **/ 
               return (
                 <CategorizedList
                   anchor={anchor}
@@ -96,17 +122,19 @@ const CategorizedListRoot = React.memo(
 );
 
 CategorizedListRoot.propTypes = {
+  // Root anchor of the form. Dots are not allowed.
   anchor: PropTypes.string,
+  // Structure of the form. Array of categories.
   categories: PropTypes.array,
+  // Array of change objects.
   changes: PropTypes.array,
-  index: PropTypes.number,
-  interval: PropTypes.number,
-  isPlaying: PropTypes.bool,
+  // Callback function that will be called when something changes on the form.
   onUpdate: PropTypes.func.isRequired,
-  sectionId: PropTypes.string,
+  // Categories might have titles. The boolean defines if are going to be shown.
   showCategoryTitles: PropTypes.bool,
-  debug: PropTypes.bool,
+  // Defines if the form can be modified.
   isReadOnly: PropTypes.bool,
+  // Boolean for showing the validation errors.
   showValidationErrors: PropTypes.bool
 };
 
