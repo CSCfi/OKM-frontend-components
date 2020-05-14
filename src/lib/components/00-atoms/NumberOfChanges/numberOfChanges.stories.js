1f8f6c030455e68c2c4b6066bda0da6d6b6e8df8
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-05-14 10:07:55 +0300

    feat: CSCOIVA-1170 localized message for NumberOfChanges

--- src/lib/components/00-atoms/NumberOfChanges/numberOfChanges.stories.js
+++ src/lib/components/00-atoms/NumberOfChanges/numberOfChanges.stories.js
@@ -4,8 +4,12 @@ import NumberOfChanges from "./index";
 import { withInfo } from "@storybook/addon-info";
 import centered from "@storybook/addon-centered/react";
 
+const messages = {
+  titleText: 'Muutosten määrä:'
+}
+
 storiesOf("NumberOfChanges", module)
   .addDecorator(centered)
   .addDecorator(withInfo)
-  .add("no changes", () => <NumberOfChanges changes={[]} />)
-  .add("9 changes", () => <NumberOfChanges changes={new Array(9)} />);
+  .add("no changes", () => <NumberOfChanges changes={[]} messages={messages} />)
+  .add("9 changes", () => <NumberOfChanges changes={new Array(9)} messages={messages} />);
