e6a78079a2d4b103594c89a2731903633dbe30ae
Raimo Pitkänen <rapitkan@gmail.com>
2020-06-11 10:39:32 +0300

    CSCOIVA-1248 (#49)
    fix: StatusTextRow's state didn't update properly.

--- dist/components/01-molecules/StatusTextRow/index.js
+++ dist/components/01-molecules/StatusTextRow/index.js
@@ -5,16 +5,16 @@ var defaultProps = {
   styleClasses: ["text-base"]
 };
 var StatusTextRow = React.memo(function (_ref) {
-  var children = _ref.children,
+  var code = _ref.code,
       isHidden = _ref.isHidden,
       isRequired = _ref.isRequired,
-      isValid = _ref.isValid,
       labelStyles = _ref.labelStyles,
       layout = _ref.layout,
       statusText = _ref.statusText,
       statusTextStyleClasses = _ref.statusTextStyleClasses,
       styleClasses = _ref.styleClasses,
-      isReadOnly = _ref.isReadOnly;
+      isReadOnly = _ref.isReadOnly,
+      title = _ref.title;
 
   var _useState = useState(defaultProps.styleClasses),
       _useState2 = _slicedToArray(_useState, 2),
@@ -41,13 +41,19 @@ var StatusTextRow = React.memo(function (_ref) {
       className: "flex"
     }, statusText && /*#__PURE__*/React.createElement("div", {
       className: R.join(" ", statusTextStyleClasses)
-    }, statusText), children, !isReadOnly && isRequired && /*#__PURE__*/React.createElement("span", {
+    }, statusText), /*#__PURE__*/React.createElement("div", {
+      className: "flex"
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "flex-1"
+    }, code ? /*#__PURE__*/React.createElement("span", {
       className: "pr-4"
-    }, "*"), " "));
+    }, code) : null, /*#__PURE__*/React.createElement("span", null, title), !isReadOnly && isRequired && /*#__PURE__*/React.createElement("span", {
+      className: "pr-4"
+    }, "*")))));
   } else {
     return /*#__PURE__*/React.createElement(React.Fragment, null);
   }
 }, function (cp, np) {
-  return cp.isHidden === np.isHidden && cp.isReadOnly === np.isReadOnly && cp.isRequired === np.isRequired && cp.isValid === np.isValid && cp.statusText === np.statusText;
+  return cp.isHidden === np.isHidden && cp.isReadOnly === np.isReadOnly && cp.isRequired === np.isRequired && cp.statusText === np.statusText && cp.code === np.code && cp.title === np.title;
 });
 export default StatusTextRow;
