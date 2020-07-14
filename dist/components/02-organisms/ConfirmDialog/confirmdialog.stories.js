790b94cf9f9befe69a20a6036ac576d2acb8e550
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-07-14 11:35:15 +0300

    feat: CSCOIVA-1268 toggleable button press spinner for ConfirmDialog ok action

--- dist/components/02-organisms/ConfirmDialog/confirmdialog.stories.js
+++ dist/components/02-organisms/ConfirmDialog/confirmdialog.stories.js
@@ -2,6 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 import ConfirmDialog from "./index";
+import { withState } from "@dump247/storybook-state";
 var messages = {
   ok: "Merkitse päätetyksi",
   cancel: "Peruuta",
@@ -9,28 +10,39 @@ var messages = {
   title: "Merkitäänkö asia päätetyksi?",
   noSave: "Älä tallenna"
 };
-storiesOf("Confirm Dialog", module).addDecorator(withInfo).add("Example 1", function () {
+storiesOf("Confirm Dialog", module).addDecorator(withInfo).add("Example 1", withState({
+  clicked: false
+})(function (_ref) {
+  var store = _ref.store;
   return /*#__PURE__*/React.createElement(ConfirmDialog, {
     isConfirmDialogVisible: true,
     handleCancel: function handleCancel() {
       return console.log("cancel");
     },
     handleOk: function handleOk() {
-      return console.log("ok");
+      return store.set({
+        clicked: true
+      });
     },
     onClose: function onClose() {
       return console.log("onClose clicked!");
     },
-    messages: messages
+    messages: messages,
+    loadingSpinner: store.state.clicked
   });
-}).add("With abandon changes", function () {
+})).add("With abandon changes", withState({
+  clicked: false
+})(function (_ref2) {
+  var store = _ref2.store;
   return /*#__PURE__*/React.createElement(ConfirmDialog, {
     isConfirmDialogVisible: true,
     handleCancel: function handleCancel() {
       return console.log("cancel");
     },
     handleOk: function handleOk() {
-      return console.log("ok");
+      return store.set({
+        clicked: true
+      });
     },
     handleExitAndAbandonChanges: function handleExitAndAbandonChanges() {
       return console.log("exit not saving");
@@ -38,6 +50,7 @@ storiesOf("Confirm Dialog", module).addDecorator(withInfo).add("Example 1", func
     onClose: function onClose() {
       return console.log("onClose clicked!");
     },
-    messages: messages
+    messages: messages,
+    loadingSpinner: store.state.clicked
   });
-});
+}));
